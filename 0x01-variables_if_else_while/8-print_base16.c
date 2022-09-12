b900a8042897bbc646a2171b650123bb3f3b9ac2
Mukhtar <mukhtaraudu31@gmail.com>
2022-09-12 10:22:49 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,49 @@
+#include <stdio.h>
 
+
+
+/**
+ *
+ * * main - entry returns 0
+ *
+ * *
+ *
+ * * Return: always returns 0
+ *
+ * *
+ *
+ */
+
+int main(void)
+
+{
+
+		unsigned char a = '0';
+
+			int i; for (i = 0; i < 10; i++)
+
+						{
+
+									putchar(a);
+
+											a++;
+
+												}
+
+					a = '1';
+
+						int 1; for (i = 0; i < 6; i++)
+
+								{
+
+											putchar('0' + a);
+
+													a++;
+
+														}
+
+							putchar('\n');
+
+								return (0);
+
+}
